39cf15f5d465119a63deb4ee120e629bb039cbbb
msa06 <suhaib.md06@gmail.com>
2022-02-07 20:09:45 +0530

    Added Contact Page

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var d = new Date();
 var y = d.getFullYear();
 document.getElementById("year").innerHTML = " " + y;
+
+// Form ELements
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
+var button = document.getElementById("submit");
+var greet = document.getElementById("greet");
+
+button.addEventListener("click", function (e) {
+  console.log(name, email);
+  greet.innerHTML = "Hello, " + name + ". Let's Meet Soon. ";
+  e.preventDefault();
+});
